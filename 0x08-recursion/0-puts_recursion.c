50f2721cbf5514dc1b19a5cfd16d664e53a4e27c
LockHacks <anathisjadudlamini@gmail.com>
2023-10-25 02:04:45 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,19 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _puts_recursion - print a string
+ * @s:string
+ * Return:void
  */
-int main(void)
+void _puts_recursion(char *s)
 {
-    _puts_recursion("Puts with recursion");
-    return (0);
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+
 }
